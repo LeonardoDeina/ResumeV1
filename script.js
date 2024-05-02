bdf52fffe49a328013fdbb2e576c37e41a4044c9
Leonardo Deina <lsd123cmc@gmail.com>
2024-05-02 15:41:10 -0300

    Alterações nas paginas e no inicio

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const navbar = document.querySelector('.navbar');
 const mobilenavbar = document.querySelector('.navbar-mobile');
 const button = document.querySelector('.bars');
-const apagafoto = function(){ this.style.display = 'none';}
-
-document.getElementById('div-foto-inicio').onclick = apagafoto
-
 
 
 button.addEventListener('click',function(){
