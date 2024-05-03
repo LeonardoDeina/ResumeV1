0028eb07e30c508d99e8bf6422636cf1e2b37980
Leonardo Deina <lsd123cmc@gmail.com>
2024-05-03 19:02:45 -0300

    Page projects adjusted

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+{
 const navbar = document.querySelector('.navbar');
 const mobilenavbar = document.querySelector('.navbar-mobile');
 const button = document.querySelector('.bars');
@@ -6,3 +7,31 @@ const button = document.querySelector('.bars');
 button.addEventListener('click',function(){
     mobilenavbar.classList.toggle('active');
 });
+}
+
+function changeProject (){
+    let projetoAtual = 0;
+    const projects = document.querySelectorAll('.Projects > div');
+    const totalProjects = projects.length;
+
+    function showProject(index) {
+        projects.forEach((projects, i) => {
+            if (i===index){
+                projects.style.display = 'flex';
+            }else{
+                projects.style.display = 'none';
+            }
+        });
+    }
+
+    function nextProject() {
+        projetoAtual = (projetoAtual+1) % totalProjects;
+        showProject(projetoAtual);
+    }
+
+    showProject(projetoAtual);
+
+    setInterval(nextProject, 10000)
+}
+
+document.addEventListener('DOMContentLoaded', changeProject);
